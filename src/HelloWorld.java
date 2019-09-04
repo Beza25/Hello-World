b5c4b596a326beb7339721bb8da500d310269a19
Beza25 <sirak.bezawit@gmail.com>
2019-09-04 11:59:22 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("Today is Sep 04, 2019. I am alive!");
+        System.out.println("My name is Emusha");
+    }
+}
